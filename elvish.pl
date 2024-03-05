877bf212b3ea3cc25c5993961932541dfcda334f
Jesus0204 <65917649+Jesus0204@users.noreply.github.com>
2024-03-04 21:31:12 -0600

    Elvish tests

--- elvish.pl
+++ elvish.pl
@@ -38,3 +38,15 @@ automatonCheck([Symbol | RestofList], InitialState) :-
     move(InitialState, NextState, Symbol),
     % If they are then call the function again until base case
     automatonCheck(RestofList, NextState).
+
+% Tests that can be run
+go_over_automaton([t, e, n, g, w, a]).
+go_over_automaton([t, e, n, g, w, e]).
+go_over_automaton([h, e, l, l, o]).
+go_over_automaton([t, i, n, c, o]).
+go_over_automaton([t, h, a, l, i, n]).
+go_over_automaton([t, h, a, l, i, a, s]).
+go_over_automaton([t, h, a, l, i, a, n]).
+go_over_automaton([t, u, i, l, ë]).
+go_over_automaton([t, u, i, l, e]).
+go_over_automaton([t, h, a, l, e]).
