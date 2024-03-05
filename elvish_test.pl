d1e2436a3e8967e50954a020a90927841d2e9fd5
Jesus0204 <65917649+Jesus0204@users.noreply.github.com>
2024-03-04 21:12:08 -0600

    Elvish tests

---
+++ elvish_test.pl
@@ -0,0 +1,10 @@
+go_over_automaton([t, e, n, g, w, a]).
+go_over_automaton([t, e, n, g, w, e]).
+go_over_automaton([h, e, l, l, o]).
+go_over_automaton([t, i, n, c, o]).
+go_over_automaton([t, h, a, l, i, n]).
+go_over_automaton([t, h, a, l, i, a, s]).
+go_over_automaton([t, h, a, l, i, a, n]).
+go_over_automaton([t, u, i, l, ë]).
+go_over_automaton([t, u, i, l, e]).
+go_over_automaton([t, h, a, l, e]).
